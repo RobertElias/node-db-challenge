f797e4243ff2baeecbadf31f7299e89a34f0d588
robertelias <robert.elias@live.com>
2020-05-15 08:27:02 -0700

    POST /api/projects/

--- api/project-router.js
+++ api/project-router.js
@@ -18,4 +18,20 @@ router.get("/", (req, res) => {
             res.status(500).json({ message: "Failed to retrieve Project Data" });
         });
 });
+
+
+/*******POST PROJECTS*********/
+//localhost:5000/api/projects/
+router.post("/", (req, res) => {
+    const newProject = req.body;
+    Projects.create(newProject)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "Error failed to create a new project" });
+        });
+});
+
 module.exports = router;
