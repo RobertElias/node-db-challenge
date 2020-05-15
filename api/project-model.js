afdf12f9d9577e6e4d3e6f5875c18af0b1b534f3
robertelias <robert.elias@live.com>
2020-05-15 08:40:12 -0700

    project model updated

---
+++ api/project-model.js
@@ -0,0 +1,12 @@
+const db = require("../data/dataConfig.js");
+module.exports = {
+    // addTasks,
+    list,
+    listById, // id
+    listTasks, // id
+    listResources,
+    listProjResources, // id
+    create,
+    createTask,
+    createResource
+};
