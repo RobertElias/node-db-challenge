2dc225ee3190d023b5fc86cd2e5da3843cd7f28b
robertelias <robert.elias@live.com>
2020-05-15 09:18:51 -0700

    resources schema

--- migrations/20200515091820_resources.js
+++ migrations/20200515091820_resources.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
-};
+    return knex.schema
+        .createTable('resources', tbl => {
+            //add an id column that auto-increments
+            tbl.increments(); //primary key
+            //resources name
+            tbl.string('resourceName', 128).unique();
+            //description
+            tbl.string('description', 500).optional();
 
+        })
+
+};
+// knex migrate:latest
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('resources')
 };
